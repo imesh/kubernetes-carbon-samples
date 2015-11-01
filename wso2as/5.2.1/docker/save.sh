2bde872e9687f39bb8fb8d7522e3c832ab94106d
Imesh Gunaratne <imesh@apache.org>
2015-11-01 03:55:58 -0800

    Updating esb sample folder structure

--- wso2as/5.2.1/docker/save.sh
+++
@@ -1,1 +0,0 @@
-docker save imesh/wso2as:5.2.1 > ~/docker-images/imesh-wso2as-5.2.1.tar
--- wso2as/5.2.1/docker/scp.sh
+++
@@ -1,3 +0,0 @@
-scp ~/docker-images/imesh-wso2as-5.2.1.tar knode1:
-ssh knode1 "docker load < imesh-wso2as-5.2.1.tar"
-ssh knode1 "rm imesh-wso2as-5.2.1.tar"
